a5781853c33701b0e4d7326d5b3229c18b3674ee
Heluzio Neto <62826121+Heluzio-Neto@users.noreply.github.com>
2023-10-24 21:19:05 -0400

    add src

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify';
+import dayjs from 'dayjs';
+import { z } from 'zod'
+import { prisma } from './lib/prisma';
+
+export async function appRoutes(app : FastifyInstance){
+
+}
+
