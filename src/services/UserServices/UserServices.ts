d7876eea1b5bc6d957a743dcdb4a17bb8a9f6824
Heluzio Neto <62826121+Heluzio-Neto@users.noreply.github.com>
2023-10-26 19:40:44 -0400

    add new params for create user

--- src/services/UserServices/UserServices.ts
+++ src/services/UserServices/UserServices.ts
@@ -6,14 +6,35 @@ import {FastifyRequest} from "fastify";
 export class UserServices {
     async CreateUser(request : FastifyRequest) {
         const createUserBody = z.object({
-            name : z.string()
+            name : z.string(),
+            surname : z.string(),
+            username : z.string(),
+            email : z.string(),
+            password : z.string(),
+            birthdate : z.coerce.date(),
+            cpf : z.string(),
         })
 
-        const { name } = createUserBody.parse(request.body);
+        const { name,
+            surname,
+            username,
+            email,
+            password,
+            birthdate,
+            cpf } = createUserBody.parse(request.body);
+            const today = dayjs().startOf('day').toDate();        
 
         await prisma.user.create({
            data : {
-            name : name
+            name : name,
+            surname : surname,
+            username: username,
+            email : email, 
+            password : password,
+            birthdate : birthdate,
+            cpf : cpf,
+            created_at : today
+
            }
         })
 
