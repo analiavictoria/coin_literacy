983b4267c0d347517e6cb2d0ab23413f8290bbf8
Heluzio Neto <62826121+Heluzio-Neto@users.noreply.github.com>
2023-10-31 01:15:39 -0400

    add new methods to blog service

--- src/services/BlogServices/BlogService.ts
+++ src/services/BlogServices/BlogService.ts
@@ -19,7 +19,6 @@ export class BlogPostServices {
                 text : z.string(),
             });
 
-    
             const { categoryPost, title, text } = createBlogPostBody.parse(req.body);
             const user  = req.user
             const today = dayjs().startOf('day').toDate();
@@ -52,5 +51,90 @@ export class BlogPostServices {
                 res.status(500).send({ error: error.message });
             }  
         }
+    }
+
+    async SearchBlogPost(req : CustomRequest, res : FastifyReply){
+        try {
+            const blogPostParam = z.object({
+                id : z.string().uuid(),
+            });         
+            const { id } = blogPostParam.parse(req.params);
+            const blogPost = await prisma.blogPosts.findFirst({ 
+                where : {
+                    id : id
+                }
+             });
+            if(!blogPost){
+                return res.status(400).send({ error : "No blog post found"})
+            }
+            return res.status(200).send({ message:"Blog post found", blogPost});
+        }catch(error){
+            if (error instanceof z.ZodError) {
+                const missingFields = error.issues.map((issue) => issue.path.join('.'));
+                res.status(400).send({ error: 'Missing fields in the request', missingFields });
+            } else {
+                res.status(500).send({ error: error.message });
+            }  
+        }
+    }
+
+    async SearchAllBlogPosts(req : CustomRequest, res : FastifyReply){
+        const allBlogPosts = await prisma.blogPosts.findMany();
+        if( allBlogPosts.length > 0 ){
+            return res.status(200).send({ message: 'Users found', allBlogPosts}); 
+        }
+        return res.status(400).send({ message: 'No registered users'}); 
+    }
+
+    async UpdateBlogPost(req : CustomRequest, res : FastifyReply){
+        try { 
+            const createBlogPostBody = z.object({
+                categoryPost : z.string().optional(),
+                title : z.string().optional(),
+                text : z.string().optional(),
+            });
+    
+            const blogPostParam = z.object({
+                id : z.string().uuid(),
+            });    
+    
+            const { categoryPost, title, text } = createBlogPostBody.parse(req.body);
+         
+            const { id } = blogPostParam.parse(req.params);
+    
+            let blogPost = await prisma.blogPosts.findFirst({ 
+                where : { 
+                    id : id 
+                }, 
+            });
+    
+            if(!blogPost){
+                return res.status(400).send({ error : "No blog post found"})
+            }
+            
+            blogPost = await prisma.blogPosts.update({
+                where : {
+                    id : id
+                },
+                data : {
+                    categoryBlogPost : categoryPost, 
+                    title : title,
+                    text : text
+                }
+            }) 
+            return res.status(200).send({ message:"Blog post Updated", blogPost});
+        }catch(error){
+            if (error instanceof z.ZodError) {
+                const missingFields = error.issues.map((issue) => issue.path.join('.'));
+                res.status(400).send({ error: 'Missing fields in the request', missingFields });
+            } else {
+                res.status(500).send({ error: error.message });
+            }  
+        }
+        
+    }
+
+    async DeleteBlogPost(req : CustomRequest, res : FastifyReply){
+
     }
 }
