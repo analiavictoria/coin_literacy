8a275240f8c123a38182274279f6551d6845c2aa
Heluzio Neto <62826121+Heluzio-Neto@users.noreply.github.com>
2023-12-14 23:14:32 -0400

    refactory

--- app/src/views/authentication/auth-forms/AuthRegister.js
+++ app/src/views/authentication/auth-forms/AuthRegister.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import api from '../../../api/api';
+import api from '../../../api/api';
 import { useNavigate } from 'react-router-dom';
 // material-ui
 import { useTheme } from '@mui/material/styles';
